a342a30f3b4b3adb7c934e49b0cf770b2296095e
thaovothu <0945206559vtt@gmail.com>
2025-10-25 23:05:24 +0700

    fix: enable Login flow

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -1,3 +1,24 @@
+// import React from 'react';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import HomeScreen from '../screens/HomeScreen/HomeScreen';
+// import LoginScreen from '../screens/LoginScreen/LoginScreen';
+// import HistoryScreen from '../screens/HistoryScreen/HistoryScreen';
+
+// const Stack = createStackNavigator();
+
+// // DEV MODE: Tắt Home và Login để code UI History
+// const DEV_MODE = false;
+
+// const AppNavigator = () => (
+//   <Stack.Navigator initialRouteName="History" screenOptions={{ headerShown: false }}>
+//     {!DEV_MODE && <Stack.Screen name="Home" component={HomeScreen} />}
+//     {!DEV_MODE && <Stack.Screen name="Login" component={LoginScreen} />}
+//     <Stack.Screen name="History" component={HistoryScreen} />
+//   </Stack.Navigator>
+// );
+
+// export default AppNavigator;
+
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
@@ -7,10 +28,10 @@ import HistoryScreen from '../screens/HistoryScreen/HistoryScreen';
 const Stack = createStackNavigator();
 
 // DEV MODE: Tắt Home và Login để code UI History
-const DEV_MODE = true;
+const DEV_MODE = false;
 
 const AppNavigator = () => (
-  <Stack.Navigator initialRouteName="History" screenOptions={{ headerShown: false }}>
+  <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
     {!DEV_MODE && <Stack.Screen name="Home" component={HomeScreen} />}
     {!DEV_MODE && <Stack.Screen name="Login" component={LoginScreen} />}
     <Stack.Screen name="History" component={HistoryScreen} />
