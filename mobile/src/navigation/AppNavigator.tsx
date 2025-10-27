40352649d2174fdd4cf6fa0f427e856f1391c361
lechieutung2003 <lechieutung2003@gmail.com>
2025-10-27 09:26:33 +0700

    feat: delete PreLogin navigation

--- mobile/src/navigation/AppNavigator.tsx
+++ mobile/src/navigation/AppNavigator.tsx
@@ -36,7 +36,7 @@ const DEV_MODE = false;
 
 const AppNavigator = () => (
   <Stack.Navigator initialRouteName="CreatOrder" screenOptions={{ headerShown: false }}>
-    {!DEV_MODE && <Stack.Screen name="PreLogin" component={PreLoginScreen} />}
+    {/* {!DEV_MODE && <Stack.Screen name="Home" component={HomeScreen} />} */}
     {!DEV_MODE && <Stack.Screen name="Login" component={LoginScreen} />}
     {!DEV_MODE && <Stack.Screen name="Register" component={RegisterScreen} />}
     {!DEV_MODE && <Stack.Screen name="ForgotPassword" component={ForgotScreen} />}
